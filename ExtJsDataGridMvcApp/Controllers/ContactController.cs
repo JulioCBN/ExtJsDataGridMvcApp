e3e2db485524700f0cd643822d44b240292d76f6
Wongso Wijaya <wswijaya@gmail.com>
2011-03-05 19:46:56 +0800

    first release

---
+++ ExtJsDataGridMvcApp/Controllers/ContactController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExtJsDataGridMvcApp.Models;
+
+namespace ExtJsDataGridMvcApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Load()
+        {
+            var contact = new List<Contact> {
+                new Contact("Smith","95746325","smith@me.com"),
+                new Contact("Adam","87291034","adam@me.com"),
+                new Contact("Eve","98271345","eve@me.com"),
+                new Contact("Chun Li","81728312","chun.li@me.com")
+            };
+            return Json(new
+            {
+                total = contact.Count,
+                data = contact,
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Create(List<Contact> data)
+        {
+            //insert Create code
+            return Json(new
+            {
+                data = new Contact(data[0].Name, data[0].Phone, data[0].Email),
+                success = true,
+                message = "Create method called successfully"
+            });
+        }
+
+        [HttpPost]
+        public JsonResult Update(List<Contact> data)
+        {
+            //insert Update code
+            return Json(new
+            {
+                success = true,
+                message = "Update method called successfully"
+            });
+        }
+
+        [HttpPost]
+        public JsonResult Delete(List<string> data)
+        {
+            //insert Delete code
+            return Json(new
+            {
+                success = true,
+                message = "Delete method called successfully"
+            });
+        }
+    }
+}
---
+++ ExtJsDataGridMvcApp/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExtJsDataGridMvcApp.Models
+{
+    public class Contact
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string pName, string pPhone, string pEmail)
+        {
+            this.Id = System.Guid.NewGuid().ToString();
+            this.Name = pName;
+            this.Phone = pPhone;
+            this.Email = pEmail;
+        }
+
+        public Contact() { }
+    }
+}
